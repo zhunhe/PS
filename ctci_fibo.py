a0bc23511513e61cdbe42b8ed4847dea9670dec4
zhunhe <xuzhunhe@gmail.com>
2020-09-20 12:20:24 +0900

    speed

--- ctci_fibo.py
+++ ctci_fibo.py
@@ -3,15 +3,29 @@
 메모이제이션을 이용한 피보나치 구하기
 """
 
-# 재귀함수의 시간복잡도는 지수시간이 걸리므로
-# 효율을 올리기위해 매번 계산하지 않고 저장, 불러오는 방식으로 설계한다
-memo = {}
+# 재귀함수 이용
 def fibo(n):
+    if n<=0:
+        return 0
+    elif n==1:
+        return 1
+    return fibo(n-2)+fibo(n-1)
+
+# 재귀함수 및 메모이제이션 이용
+memo = {}
+def fibo_memoi(n):
     if n<=1:
         memo[n]=n
     elif n not in memo:
-        memo[n] = fibo(n-2) + fibo(n-1)
+        memo[n] = fibo_memoi(n-2) + fibo_memoi(n-1)
     return memo[n]
 
-# n-1번째 피보나치 수 구하기
-print(fibo(5))
+from timeit import *
+t1 = Timer("fibo_memoi(40)", "from __main__ import fibo_memoi")
+t2 = Timer("fibo(40)", "from __main__ import fibo")
+print("fibo_memoi(40): ", t1.timeit(number=1),"seconds")
+print("fibo(40): ", t2.timeit(number=1),"seconds")
+
+#시간비교
+#fibo_memoi(40):  3.895699999999919e-05 seconds
+#fibo(40):  51.378306183 seconds
