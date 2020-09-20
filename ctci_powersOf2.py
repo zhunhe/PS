a315c9221be1747427d0b1ae25b1403e75003559
zhunhe <xuzhunhe@gmail.com>
2020-09-20 12:35:35 +0900

    Create

---
+++ ctci_powersOf2.py
@@ -0,0 +1,17 @@
+#ctci_powersOf2.py
+
+def powersOf2(n):
+    if n<1:
+        return 0
+    elif n==1:
+        print(1)
+        return 1
+    else:
+        prev = int(powersOf2(int(n/2)))
+        curr = prev*2
+        print(curr)
+        return curr
+
+powersOf2(50)
+
+#O(logn)
