40ca23e5e80616d307d9724e8856ed1c299b19ee
zhunhe <xuzhunhe@gmail.com>
2020-12-17 19:26:23 +0900

    Make function

--- rotto.py
+++ rotto.py
@@ -2,10 +2,13 @@
 
 import random
 
-result=[]
-while len(result)<6:
-    num=random.randint(1,45)
-    if num not in result:
-        result.append(num)
+def rotto():
+    result=[]
+    while len(result)<6:
+        num=random.randint(1,45)
+        if num not in result:
+            result.append(num)
 
-print(result)
+    print(result)
+
+rotto()
