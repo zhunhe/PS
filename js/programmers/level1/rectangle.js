6cae76091e1a9631f14e7c1810249d5295edd175
zhunhe <xuzhunhe@gmail.com>
2021-09-28 09:59:00 +0900

    Append better code to rectangle.js

--- js/programmers/level1/rectangle.js
+++ js/programmers/level1/rectangle.js
@@ -19,3 +19,17 @@ process.stdin.on('data', data => {
         console.log();
     }
 });
+
+/*
+[다른 사람 풀이]
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
+    }
+
+});
+ */
