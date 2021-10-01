1bc64c81d3c010ef0ec099e2fa78cc337e75f0d5
zhunhe <xuzhunhe@gmail.com>
2021-10-01 17:45:47 +0900

    프로그래머스 Lv.1 문제

---
+++ js/programmers/level1/harshad.js
@@ -0,0 +1,31 @@
+/*
+문제 설명
+양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다.
+자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+
+제한 조건
+x는 1 이상, 10000 이하인 정수입니다.
+*/
+
+function solution(x) {
+  let num = 0;
+  let x_org = x;
+  while (parseInt(x) > 0) {
+      num += parseInt(x) % 10;
+      x = parseInt(x) / 10;
+  }
+  if (x_org % num == 0)
+      return true;
+  return false;
+}
+
+/* 다른 사람의 풀이
+function Harshad(n){
+  return !(n % (n + "").split("").reduce((a, b) => +b + +a ));
+}
+다른 사람의 풀이2
+function Harshad(n){
+  return !(n%(n+'').split('').reduce(function (i, sum) {return +sum + +i;}));
+}
+*/
---
+++ js/programmers/level1/sum-of-matrix.js
@@ -0,0 +1,29 @@
+/*
+문제 설명
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의
+같은 행, 같은 열의 값을 서로 더한 결과가 됩니다.
+2개의 행렬 arr1과 arr2를 입력받아,
+행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+제한 조건
+행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+*/
+
+function solution(arr1, arr2) {
+  var answer = [];
+  var tmp = [];
+  for (let i = 0; i < arr1.length; i++) {
+      tmp = [];
+      for (let j = 0; j < arr1[0].length; j++) {
+          tmp.push(arr1[i][j]+arr2[i][j]);
+      }
+      answer.push(tmp);
+  }
+  return answer;
+}
+
+/* 다른 사람의 풀이
+function solution(A,B){
+    return A.map((a,i) => a.map((b, j) => b + B[i][j]));
+}
+*/
