89e48e7c89744b928b954cb817da4ddedb2aa5b0
zhunhe <xuzhunhe@gmail.com>
2021-10-13 20:10:18 +0900

    feat: Solve boj 1931 using greedy

---
+++ js/boj/1931-meeting_room.js
@@ -0,0 +1,35 @@
+/*
+[boj] 회의실 배정(1931번)
+https://www.acmicpc.net/problem/1931
+*/
+
+// 파일로부터 값 입력 받기
+const fs = require("fs");
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './testcase/1931.txt';
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const n = parseInt(input[0]);
+
+// 받아온 값 세팅
+let nums = [];
+for (let i = 1; i < n + 1; i++) {
+    let st = parseInt(input[i].split(" ")[0]);
+    let en = parseInt(input[i].split(" ")[1]);
+    nums[i - 1] = [st, en];
+}
+
+nums.sort((a, b) => {
+    if (a[1] === b[1])
+        return a[0] - b[0];
+    else
+        return a[1] - b[1];
+});
+
+let et = 0;
+let answer = 0;
+for (x of nums) {
+    if (x[0] >= et) {
+        answer++;
+        et = x[1];
+    }
+}
+console.log(answer);
