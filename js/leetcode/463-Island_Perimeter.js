247e668d4c24150d474a3dc2e0c070141bbcd5fb
zhunhe <xuzhunhe@gmail.com>
2021-10-15 11:40:06 +0900

    feat: Solve leetcode 463 using dfs

--- js/leetcode/463-Island_Perimeter.js
+++ js/leetcode/463-Island_Perimeter.js
@@ -3,8 +3,9 @@
 https://leetcode.com/problems/island-perimeter/
 */
 
+// iterative
 var islandPerimeter = function (grid) {
-    let line = 0;
+    let answer = 0;
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
@@ -12,20 +13,48 @@ var islandPerimeter = function (grid) {
                 continue;
             }
             if (j === 0 || grid[i][j - 1] === 0) {
-                line++;
+                answer++;
             }
             if (j === grid[i].length - 1 || grid[i][j + 1] === 0) {
-                line++;
+                answer++;
             }
             if (i === 0 || grid[i - 1][j] === 0) {
-                line++;
+                answer++;
             }
             if (i === grid.length - 1 || grid[i + 1][j] === 0) {
-                line++;
+                answer++;
             }
         }
     }
-    return line;
+    return answer;
+};
+
+// dfs
+var islandPerimeterDFS = function (grid) {
+    let answer = 0;
+    let len = grid.length;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                DFS(i, j);
+            }
+        }
+    }
+    function DFS(i, j) {
+        grid[i][j] = 2; // visited
+
+        if (j === 0 || grid[i][j - 1] === 0) answer++;
+        if (j === len - 1 || grid[i][j + 1] === 0) answer++;
+        if (i === 0 || grid[i - 1][j] === 0) answer++;
+        if (i === len - 1 || grid[i + 1][j] === 0) answer++;
+
+        if (i > 0 && grid[i - 1][j] === 1) DFS(i - 1, j);
+        if (j > 0 && grid[i][j - 1] === 1) DFS(i, j - 1);
+        if (i < len - 1 && grid[i + 1][j] === 1) DFS(i + 1, j);
+        if (j < len - 1 && grid[i][j + 1] === 1) DFS(i, j + 1);
+    }
+    return answer;
 };
 
 testcases = [
@@ -41,5 +70,5 @@ testcases = [
 output = [16, 4, 4];
 
 for (let i = 0; i < output.length; i++) {
-    console.log(output[i], islandPerimeter(testcases[i]));
+    console.log(output[i], islandPerimeterDFS(testcases[i]));
 }
