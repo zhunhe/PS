e39394b92aa72ff2de94735e2e3e48bbbfb144fd
zhunhe <xuzhunhe@gmail.com>
2021-11-04 21:49:11 +0900

    feat: Solve boj 13305 using greedy

--- js/boj/13305-gas_station.js
+++ js/boj/13305-gas_station.js
@@ -5,27 +5,18 @@ https://www.acmicpc.net/problem/13305
 
 // 파일로부터 값 입력 받기
 const fs = require("fs");
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './testcase/13305.txt';
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./testcase/13305.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-// 도시 갯수
-const n = parseInt(input[0]);
+const n = +input[0];
+const distance = input[1].split(" ").map((v) => BigInt(v));
+const price = input[2].split(" ").map((v) => BigInt(v));
 
-// 도시간 거리
-let len = [];
+let curPrice = price[0];
+let cost = 0n;
+
 for (let i = 0; i < n - 1; i++) {
-    len.push(parseInt(input[1].split(" ")[i]));
+	if (curPrice > price[i]) curPrice = price[i];
+	cost += curPrice * distance[i];
 }
-// 기름 가격
-let price = [];
-for (let i = 0; i < n; i++) {
-    price.push(parseInt(input[2].split(" ")[i]));
-}
-// 가격 계산
-let min_price = price[0];
-let answer = 0;
-for (let i = 0; i < len.length; i++) {
-    min_price = Math.min(min_price, price[i]);
-    answer += parseInt(min_price) * parseInt(len[i]);
-}
-console.log(answer);
+console.log(String(cost));
