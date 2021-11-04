9e3c99a5c548a9a9d010fb55a9d6052ae124e0c1
zhunhe <xuzhunhe@gmail.com>
2021-11-04 21:17:08 +0900

    hi

--- js/boj/13305-gas_station.js
+++ js/boj/13305-gas_station.js
@@ -5,27 +5,19 @@ https://www.acmicpc.net/problem/13305
 
 // 파일로부터 값 입력 받기
 const fs = require("fs");
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './testcase/13305.txt';
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./testcase/13305.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-// 도시 갯수
 const n = parseInt(input[0]);
+const DIST = 1;
+const PRICE = 2;
 
-// 도시간 거리
-let len = [];
-for (let i = 0; i < n - 1; i++) {
-    len.push(parseInt(input[1].split(" ")[i]));
-}
-// 기름 가격
-let price = [];
-for (let i = 0; i < n; i++) {
-    price.push(parseInt(input[2].split(" ")[i]));
-}
-// 가격 계산
-let min_price = price[0];
 let answer = 0;
-for (let i = 0; i < len.length; i++) {
-    min_price = Math.min(min_price, price[i]);
-    answer += parseInt(min_price) * parseInt(len[i]);
+let oilPrice = BigInt(input[PRICE].split(" ")[0]);
+console.log(oilPrice);
+for (let i = 0; i < n - 1; i++) {
+	if (BigInt(input[PRICE].split(" ")[i]) < oilPrice) oilPrice = BigInt(input[PRICE].split(" ")[i]);
+	let distance = BigInt(input[DIST].split(" ")[i]);
+	answer += distance * oilPrice;
 }
 console.log(answer);
