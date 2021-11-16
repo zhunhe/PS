102b298b59861bae5edd2d81181dfa1e97dc41e6
zhunhe <xuzhunhe@gmail.com>
2021-11-16 18:36:26 +0900

    feat: Solve boj 9663 backtracking problem

---
+++ c++/boj/9663.cpp
@@ -0,0 +1,39 @@
+/*
+ * N-Queen
+ * https://www.acmicpc.net/problem/9663
+ */
+
+#include <iostream>
+using namespace std;
+int N, CNT;
+int BOARD[14];
+
+bool isValid(int idx) {
+    for (int i = 0; i < idx; i++) {
+        if (BOARD[idx] == BOARD[i]) return false;
+        if (idx - i == abs(BOARD[idx] - BOARD[i])) return false;
+    }
+    return true;
+}
+
+void nqueen(int L) {
+    if (L == N) {
+        CNT++;
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        BOARD[L] = i;
+        if (isValid(L)) {
+            nqueen(L + 1);
+        }
+    }
+}
+
+int main() {
+    cin >> N;
+    if (N >= 1 && N < 15) {
+        nqueen(0);
+        cout << CNT;
+    }
+    return 0;
+}
