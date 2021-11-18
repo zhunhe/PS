2a4f7aef04eb79e8eb60d5e50ae1c06976062f4b
zhunhe <xuzhunhe@gmail.com>
2021-11-18 19:21:52 +0900

    feat: Solve boj 9012.cpp stack problem

---
+++ c++/boj/9012.cpp
@@ -0,0 +1,39 @@
+/*
+ * 괄호
+ * https://www.acmicpc.net/problem/9012
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int T;
+
+int main() {
+    cin >> T;
+    while (T) {
+        string brace;
+        cin >> brace;
+
+        int cnt = 0;
+        bool flag = true;
+        for (int i = 0; i < brace.length(); i++) {
+            if (brace[i] == '(') {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+            if (cnt < 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag && cnt == 0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+        T--;
+    }
+}
