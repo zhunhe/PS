b60d73ad49332e5ca1e504fd63742e76b5d5587b
zhunhe <xuzhunhe@gmail.com>
2021-12-04 11:43:40 +0900

    feat: Solve 1931 greedy problem

---
+++ c++/boj/1931.cpp
@@ -0,0 +1,49 @@
+/*
+ * 회의실 배정
+ * https://www.acmicpc.net/problem/1931
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<pair<int, int> > v;
+vector<int> answer;
+
+bool compare(pair<int, int> v1, pair<int, int> v2) {
+	if (v1.second == v2.second)
+		return v1.first < v2.first;
+	return v1.second < v2.second;
+}
+
+void set_input_data() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+	int start, end;
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> start >> end;
+		v.push_back(make_pair(start, end));
+    }
+}
+
+void solve_DP() {
+	sort(v.begin(), v.end(), compare);
+	answer.push_back(v[0].second);
+	for (int i = 1; i < N; i++) {
+		if (answer[answer.size() - 1] <= v[i].first) {
+			answer.push_back(v[i].second);
+		}
+	}
+}
+
+int main() {
+    set_input_data();
+	solve_DP();
+	cout << answer.size();
+    return 0;
+}
