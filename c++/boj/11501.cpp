48e94159a633ade6f5f7a4e55f51ede230df0790
zhunhe <xuzhunhe@gmail.com>
2021-12-09 10:28:01 +0900

    refactor: Update 11501.cpp
    - Add comment for algorithm description
    - Change function name solve_GREEDY -> GREEDY
    - Change variable name max -> max_val

--- c++/boj/11501.cpp
+++ c++/boj/11501.cpp
@@ -24,24 +24,34 @@ vector<int> set_input_data() {
 	return stock;
 }
 
-long long solve_GREEDY(vector<int> stock) {
-	int max = stock[stock.size() - 1];
+// ❗️주식을 구매, 판매하여 가장 높은 수익을 올리면 얼만지 구하는 문제
+// 하루에 아래 중 하나의 행동을 할 수 있음
+// 1. 주식 하나를 산다.
+// 2. 원하는 만큼 가지고 있는 주식을 판다.
+// 3. 아무것도 안한다.
+long long GREEDY(vector<int> stock) {
+	// 마지막값을 초기값으로 설정
+	int max_val = stock[stock.size() - 1];
 	stack<int> tmp;
 	long long answer = 0;
 
+	// 뒤에서부터 스택에 현재 max_val보다 큰 가격이 나오기 전까지 주식가격을 push
+	// max_val보다 큰 가격이 나올 경우 'max_val - 스택에 들어있는 가격'을 answer에 저장
 	for (int i = stock.size() - 2; i >= 0; i--) {
-		if (stock[i] >= max) {
+		if (stock[i] >= max_val) {
 			while (tmp.size()) {
-				answer += (max - tmp.top());
+				answer += (max_val - tmp.top());
 				tmp.pop();
 			}
-			max = stock[i];
-		} else {
-			tmp.push(stock[i]);
+			max_val = stock[i];
+			continue;
 		}
+		tmp.push(stock[i]);
 	}
+	// 0번 인덱스까지 도착했는데 스택에 값이 남아있는 경우
+	// max_val보다 큰 가격이 나올 경우 'max_val - 스택에 들어있는 가격'을 answer에 저장
 	while (tmp.size()) {
-		answer += (max - tmp.top());
+		answer += (max_val - tmp.top());
 		tmp.pop();
 	}
 	return answer;
@@ -52,7 +62,7 @@ int main() {
 
 	cin >> t;
 	while (t--) {
-		cout << solve_GREEDY(set_input_data()) << '\n';
+		cout << GREEDY(set_input_data()) << '\n';
 	}
 	return 0;
 }
