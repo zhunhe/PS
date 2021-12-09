5d63936d39430d9039ef06230f77d6a3dedf9e8c
zhunhe <xuzhunhe@gmail.com>
2021-12-09 09:44:19 +0900

    refactor: Update 1439.cpp
    - Add comment for algorithm description
    - Change function name solve_GREEDY -> GREEDY
    - Change variable name S -> s

--- c++/boj/1439.cpp
+++ c++/boj/1439.cpp
@@ -7,31 +7,35 @@
 #include <string>
 using namespace std;
 
-string S;
+string s;
 
 void set_input_data() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> S;
+	cin >> s;
 }
 
-int solve_GREEDY() {
+// ❗️0, 1로 이루어진 문자열이 주어졌을 때 모두 같은 숫자로 만들기 위한 최소횟수
+// 이어진 숫자들은 한 번에 바꿀 수 있음
+int GREEDY() {
 	int zero_cnt = 0;
 	int one_cnt = 0;
-	int len = S.length();
+	int len = s.length();
 
 	for (int i = 0; i < len; i++) {
-		if (i == 0 || S[i] != S[i - 1]) {
-			if (S[i] == '0') zero_cnt++;
+		// 전체 s에서 이어진 0, 1의 개수를 카운트해줌
+		if (i == 0 || s[i] != s[i - 1]) {
+			if (s[i] == '0') zero_cnt++;
 			else one_cnt++;
 		}
 	}
+	// 카운트 해준 값 중 더 작은 값을 리턴
 	return (zero_cnt >= one_cnt ? one_cnt : zero_cnt);
 }
 
 int main() {
 	set_input_data();
-	cout << solve_GREEDY();
+	cout << GREEDY();
 	return 0;
 }
