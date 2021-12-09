30242ee164d9b25f19f5e2c17b3255b0751a9ef5
zhunhe <xuzhunhe@gmail.com>
2021-12-09 10:36:35 +0900

    refactor: Update 13305.cpp
    - Add comment for algorithm description
    - Change function name solve_GREEDY -> GREEDY

--- c++/boj/13305.cpp
+++ c++/boj/13305.cpp
@@ -21,12 +21,15 @@ void set_input_data() {
 		cin >> OIL[i];
 }
 
-long long solve_GREEDY() {
+// ❗️다음 마을까지의 거리, 주유소의 가격을 입력받아 이동에 필요한 최소금액을 구하는 문제
+long long GREEDY() {
 	long long answer = 0;
 	long long price = OIL[0];
 
 	for (int i = 0; i < N - 1; i++) {
+		// 최소 주유가격을 갱신
 		price = min(price, OIL[i]);
+		// 다음 마을까지의 거리 * 오일가격을 answer에 더해줌
 		answer += price * DISTANCE[i];
 	}
 	return answer;
@@ -34,6 +37,6 @@ long long solve_GREEDY() {
 
 int main() {
 	set_input_data();
-	cout << solve_GREEDY();
+	cout << GREEDY();
 	return 0;
 }
