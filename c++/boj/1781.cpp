9736e88fa28f284f88ee7e878596134cc557ffd4
zhunhe <xuzhunhe@gmail.com>
2021-12-09 10:33:38 +0900

    refactor: Update 17811.cpp
    - Add comment for algorithm description
    - Change function name solve_GREEDY -> GREEDY

--- c++/boj/1781.cpp
+++ c++/boj/1781.cpp
@@ -32,16 +32,21 @@ bool compare(pair<int, long long> front, pair<int, long long> back) {
 	return false;
 }
 
-long long solve_GREEDY() {
+// ❗️문제의 데드라인과 해결시의 받을 수 있는 컵라면들의 정보가 있는 데이터를 받아
+// 최대로 받을 수 있는 컵라면 수를 구하는 문제
+long long GREEDY() {
 	long long answer = 0;
 	int i = 0;
 
+	// 데드라인기준으로 내림차순 정렬
 	sort(v.begin(), v.end(), compare);
 	int deadline_max = v[0].first;
 	while (deadline_max) {
+		// 현재날짜와 데드라인이 같으면 max_heap에 push
 		while (v[i].first == deadline_max) {
 			pq.push(v[i++].second);
 		}
+		// 현재날짜중 가장 컵라면을 많이 받을 수 있는 문제를 선택
 		if (pq.size()) {
 			answer += pq.top();
 			pq.pop();
@@ -53,6 +58,6 @@ long long solve_GREEDY() {
 
 int main() {
 	set_input_data();
-	cout << solve_GREEDY();
+	cout << GREEDY();
 	return 0;
 }
