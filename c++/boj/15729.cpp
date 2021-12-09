4c900f08cfe364a15c80d7184f9199d5b7fe2613
zhunhe <xuzhunhe@gmail.com>
2021-12-09 10:51:23 +0900

    refactor: Update 15729.cpp
    - Add comment for algorithm description
    - Change function name solve_GREEDY -> GREEDY
    - Change variable to lowercase
    - Use XOR bitwise operator

--- c++/boj/15729.cpp
+++ c++/boj/15729.cpp
@@ -7,30 +7,32 @@
 #include <algorithm>
 using namespace std;
 
-int N;
-int LIGHT[1000000];
+int n;
+int light[1000000];
 
 void set_input_data() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> LIGHT[i];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> light[i];
 	}
 }
 
-int solve_GREEDY() {
+// ❗️모든 불이 꺼져있는 상태에서 입력받은 형태로 불을 켜기위해 누르는 버튼의 최소값을 구하는 문제
+// 버튼을 누를시 뒤에 2개 버튼도 같이 눌림
+// 모두 꺼진 상태에서 켜기 위한 횟수와 켜진 상태에서 모두 끄기 위해 누르는 버튼 횟수는 같음
+int GREEDY() {
 	int answer = 0;
 
-	for (int i = 0; i < N; i++) {
-		if (LIGHT[i] == 0) continue;
+	// 앞에서부터 켜진 것만 찾아서 꺼주면서 횟수를 기록하면 문제풀이 끝
+	for (int i = 0; i < n; i++) {
+		if (light[i] == 0) continue;
 		answer++;
-		for (int j = 0; j < 3 && j < N; j++) {
-			if (LIGHT[i + j] == 1)
-				LIGHT[i + j] = 0;
-			else
-				LIGHT[i + j] = 1;
+		for (int j = 0; j < 3 && j < n; j++) {
+			// 켜진건 끄고 꺼진건 켜고
+			light[i + j] ^= 1;
 		}
 	}
 	return answer;
@@ -38,6 +40,6 @@ int solve_GREEDY() {
 
 int main() {
 	set_input_data();
-	cout << solve_GREEDY();
+	cout << GREEDY();
 	return 0;
 }
