16bb1339e73ce7bf0f60fa89ffbe4b60ec2e3669
zhunhe <xuzhunhe@gmail.com>
2021-12-11 19:06:45 +0900

    refactor: Update 2293.cpp
    - Change function name solve_DP -> DP
    - Change varialbe name DP -> dp

--- c++/boj/2293.cpp
+++ c++/boj/2293.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int N, K;
 int COIN[101];
-int DP[10001];
+int dp[10001];
 
 // set_input_data() is a function that stores data received as a standard input in a variable.
 void set_input_data() {
@@ -24,13 +24,14 @@ void set_input_data() {
 
 // solve_DP() is a function that solves problem using dynamic programming technique.
 // Coin change problem number of ways to get total
-void solve_DP() {
-    DP[0] = 1;
+int DP() {
+    dp[0] = 1;
     for (int i = 0; i < N; i++) {
         for (int j = COIN[i]; j <= K; j++) {
-            DP[j] += DP[j - COIN[i]];
+            dp[j] += dp[j - COIN[i]];
         }
     }
+	return dp[K];
 }
 
 // main() is main function.
@@ -38,7 +39,6 @@ void solve_DP() {
 // solve_DP(): solve problem using DP algorithm
 int main() {
     set_input_data();
-    solve_DP();
-    cout << DP[K];
+    cout << DP();
     return 0;
 }
