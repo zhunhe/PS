4e343a17264601e4134a0da88961e6a200b6a9c1
zhunhe <xuzhunhe@gmail.com>
2021-12-13 09:10:54 +0900

    refactor: Update 2293.cpp
    - Delete comment
    - Delete functions except for main
    - Using FAST_IO()

--- c++/boj/2293.cpp
+++ c++/boj/2293.cpp
@@ -3,42 +3,22 @@
  * https://www.acmicpc.net/problem/2293
  */
 
+#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int N, K;
-int COIN[101];
-int dp[10001];
-
-// set_input_data() is a function that stores data received as a standard input in a variable.
-void set_input_data() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> N >> K;
-    for (int i = 0; i < N; i++) {
-        cin >> COIN[i];
-    }
-}
-
-// solve_DP() is a function that solves problem using dynamic programming technique.
-// Coin change problem number of ways to get total
-int DP() {
-    dp[0] = 1;
-    for (int i = 0; i < N; i++) {
-        for (int j = COIN[i]; j <= K; j++) {
-            dp[j] += dp[j - COIN[i]];
-        }
-    }
-	return dp[K];
-}
-
-// main() is main function.
-// set_input_data(): set data to 1-d array from standard input
-// solve_DP(): solve problem using DP algorithm
 int main() {
-    set_input_data();
-    cout << DP();
+	FAST_IO();
+	int n, k;	cin >> n >> k;
+	vector<int> coin(n);
+	for (int i = 0; i < n; i++)	cin >> coin[i];
+	vector<int> dp(k + 1);
+	dp[0] = 1;
+	for (int i = 0; i < n; i++)
+		for (int j = coin[i]; j <= k; j++)
+			dp[j] += dp[j - coin[i]];
+    cout << dp[k];
     return 0;
 }
