142c976377fe41ef5535fa91ad68f2036e04f1f0
zhunhe <xuzhunhe@gmail.com>
2021-12-14 14:33:16 +0900

    refactor: Update 2805.cpp
    - Delete FAST_IO()
    - Delete ternary operator
    - Delete namespace std;

--- c++/boj/2805.cpp
+++ c++/boj/2805.cpp
@@ -3,29 +3,26 @@
  * https://www.acmicpc.net/problem/2805
  */
 
-#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 typedef long long	ll;
 
 int main() {
-	FAST_IO();
-	int n, m;	cin >> n >> m;
-	vector<ll> log(n);
-	for (int i = 0; i < n; i++) cin >> log[i];
+	int n, m;	std::cin >> n >> m;
+	std::vector<ll> log(n);
+	for (int i = 0; i < n; i++) std::cin >> log[i];
 	ll left = 1, right = *max_element(log.begin(), log.end());
 	ll ans = 0;
 	while (left <= right) {
 		ll mid = (left + right) / 2;
 		ll cut = 0;
 		for (int i = 0; i < n; i++)
-			cut += log[i] >= mid ? log[i] - mid : 0;
+			cut += std::max(log[i] - mid, 0LL);
 		if (cut >= m) {
 			left = mid + 1;
 			ans = mid;
 		} else	right = mid - 1;
 	}
-	cout << ans;
+	std::cout << ans;
 }
