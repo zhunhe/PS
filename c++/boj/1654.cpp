36fae0b548af77280ad5ebfe9cd24ef511de9ae6
zhunhe <xuzhunhe@gmail.com>
2021-12-14 12:16:38 +0900

    refactor: Update 1654.cpp
    - Delete FAST_IO()
    - Use typedef long long ll

--- c++/boj/1654.cpp
+++ c++/boj/1654.cpp
@@ -3,28 +3,26 @@
  * https://www.acmicpc.net/problem/1654
  */
 
-#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
 #include <algorithm>
 #include <vector>
+typedef long long    ll
 
 int main() {
-	FAST_IO();
 	int k, n; std::cin >> k >> n;
 	std::vector<int> line(k);
-	for (int i = 0; i < k; i++)	std::cin >> line[i];
-	long left = 1;	long right = *max_element(line.begin(), line.end());
-	long ans = 0;
+	for (int i = 0; i < k; i++) std::cin >> line[i];
+	ll left = 1; ll right = *max_element(line.begin(), line.end());
+	ll ans = 1;
 	while (left <= right) {
-		long mid = (left + right) / 2;
+		ll mid = (left + right) / 2;
 		int cnt = 0;
 		for (int i = 0; i < k; i++)
 			cnt += line[i] / mid;
 		if (cnt >= n) {
 			ans = mid;
 			left = mid + 1;
-		} else
-			right = mid - 1;
+		} else right = mid - 1;
 	}
 	std::cout << ans;
 }
