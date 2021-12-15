a50d7b58918eae6c1367c09e1aea3b3ad0245f64
zhunhe <xuzhunhe@gmail.com>
2021-12-15 14:37:35 +0900

    refactor: Update 15810.cpp
    - Delete FAST_IO()
    - Delete variable min

--- c++/boj/15810.cpp
+++ c++/boj/15810.cpp
@@ -3,26 +3,22 @@
  * https://www.acmicpc.net/problem/15810
  */
 
-#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
-#include <algorithm>
 #include <vector>
-typedef long long	ll;
+#include <algorithm>
+typedef long long    ll;
 
 int main() {
-	FAST_IO();
-	int n, m;	std::cin >> n >> m;
-	std::vector<int> staff(n);
-	for (int i = 0; i < n; i++) std::cin >> staff[i];
-	ll min = *std::min_element(staff.begin(), staff.end());
-	ll left = 0;	ll right = min * m;
-	ll ans = 0;
+	int n, m;    std::cin >> n >> m;
+	std::vector<int> time(n);
+	for (int i = 0; i < n; i++) std::cin>> time[i];
+	ll ans, left = 1, right = (ll)*std::min_element(time.begin(), time.end()) * m;
 	while (left <= right) {
 		ll mid = (left + right) / 2;
-		ll sum = 0;
+		ll cnt = 0;
 		for (int i = 0; i < n; i++)
-			sum += mid / staff[i];
-		if (sum >= m) {
+			cnt += mid / time[i];
+		if (cnt >= m) {
 			ans = mid;
 			right = mid - 1;
 		} else left = mid + 1;
