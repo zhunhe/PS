fcf9fd4379bb7f30e407c8cd4df80f22324f3eaa
zhunhe <xuzhunhe@gmail.com>
2021-12-15 13:05:48 +0900

    refactor: Update 2512.cpp
    - Delete FAST_IO()
    - Change initial value of right variable

--- c++/boj/2512.cpp
+++ c++/boj/2512.cpp
@@ -3,33 +3,25 @@
  * https://www.acmicpc.net/problem/2512
  */
 
-#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 int main() {
-	FAST_IO();
-	int n;	std::cin >> n;
+	int n;    std::cin >> n;
 	std::vector<int> budget(n);
-	for (int i = 0; i < n; i++)	std::cin >> budget[i];
-	int m;	std::cin >> m;
-	int left = 1;	int right = m;
-	int ans = left;
+	for (int i = 0; i < n; i++) std::cin >> budget[i];
+	int m;    std::cin >> m;
+	int ans, left = 1, right = *std::max_element(budget.begin(), budget.end());
 	while (left <= right) {
 		int mid = (left + right) / 2;
 		int sum = 0;
-		for (int i = 0; i < n; i++) {
-			if (budget[i] < mid) sum += budget[i];
-			else sum += mid;
-		}
-		if (sum > m) right = mid - 1;
-		else {
-			left = mid + 1;
+		for (int i = 0; i < n; i++)
+			sum += std::min(mid, budget[i]);
+		if (sum <= m) {
 			ans = mid;
-		}
+			left = mid + 1;
+		} else right = mid - 1;
 	}
-	int max = *max_element(budget.begin(), budget.end());
-	if (ans > max) std::cout << max;
-	else std::cout << ans;
+	std::cout << ans;
 }
