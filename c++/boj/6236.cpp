82b09f19e3bb9730f69d60b5250c89edcc66e290
zhunhe <xuzhunhe@gmail.com>
2021-12-16 16:49:14 +0900

    feat: Solve boj 6236 binary search problem

---
+++ c++/boj/6236.cpp
@@ -0,0 +1,37 @@
+/*
+ * 용돈 관리
+ * https://www.acmicpc.net/problem/6236
+ */
+
+#include <iostream>
+#include <vector>
+
+bool func(int mid, int n, int m, std::vector<int> money) {
+	int num = 1;
+	int sum = mid;
+	for (int i = 0; i < n; i++) {
+		if (mid < money[i])
+			return false;
+		if (sum - money[i] < 0) {
+			sum = mid;
+			num++;
+		}
+		sum -= money[i];
+	}
+	return m >= num;
+}
+
+int main() {
+	int n, m;	std::cin >> n >> m;
+	std::vector<int> money(n);
+	for (auto &elem : money) std::cin >> elem;
+	int ans = 0, left = 1, right = 1e9;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (func(mid, n, m, money)) {
+			ans = mid;
+			right = mid - 1;
+		} else left = mid + 1;
+	}
+	std::cout << ans;
+}
