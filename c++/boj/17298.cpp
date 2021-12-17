e230b96eb2a96d6625ee241a0219e5fd6dbad9ec
zhunhe <xuzhunhe@gmail.com>
2021-12-17 19:44:45 +0900

    feat: Solve boj 17298 stack problem

---
+++ c++/boj/17298.cpp
@@ -0,0 +1,24 @@
+/*
+ * ¿ÀÅ«¼ö
+ * https://www.acmicpc.net/problem/17298
+ */
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+int main() {
+	int n;	std::cin >> n;
+	std::vector<int> num(n), ans(n, -1);
+	for (int &elem : num) std::cin >> elem;
+	std::stack<int> st;
+	for (int i = 0; i < n; i++) {
+		while (st.size() && num[i] > num[st.top()]) {
+			ans[st.top()] = num[i];
+			st.pop();
+		}
+		st.push(i);
+	}
+	for (auto elem: ans)
+		std::cout << elem << ' ';
+}
