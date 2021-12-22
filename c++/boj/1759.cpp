c5015dcd72cbf3d84f8ea25ce01cb88972fa155f
zhunhe <xuzhunhe@gmail.com>
2021-12-22 19:02:20 +0900

    feat: Solve boj 1759 DFS problem

---
+++ c++/boj/1759.cpp
@@ -0,0 +1,50 @@
+/*
+ * ¿¬±¸¼Ò
+ * https://www.acmicpc.net/problem/14502
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int l, c;
+char alpha[15];
+vector<char> tmp;
+
+bool is_valid(vector<char> s) {
+	int c_count = 0, v_count = 0;
+	for (auto c : s) {
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+			v_count++;
+		else
+			c_count++;
+	}
+	if (v_count >= 1 && c_count >= 2)
+		return true;
+	return false;
+}
+
+void DFS(int L, int s) {
+	if (L == l) {
+		if (is_valid(tmp)) {
+			for (int i = 0; i < l; i++)
+				cout << tmp[i];
+			cout << '\n';
+		}
+		return;
+	}
+	for (int i = s; i < c; i++) {
+		tmp.push_back(alpha[i]);
+		DFS(L + 1, i + 1);
+		tmp.pop_back();
+	}
+}
+
+int main() {
+	cin >> l >> c;
+	for (int i = 0; i < c; i++) cin >> alpha[i];
+	sort(alpha, alpha + c);
+	DFS(0, 0);
+}
