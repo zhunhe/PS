745dbcd61a91578f421399fac27c265ad859c4da
zhunhe <xuzhunhe@gmail.com>
2021-12-22 20:49:49 +0900

    feat: Solve boj 9935 stack problem

---
+++ c++/boj/9935.cpp
@@ -0,0 +1,30 @@
+/*
+ * ¹®ÀÚ¿­ Æø¹ß
+ * https://www.acmicpc.net/problem/9935
+ */
+
+#define FAST_IO()	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	string s1, s2;	cin >> s1 >> s2;
+	vector<char> ans;
+	int s2_len = s2.length();
+	for (auto c : s1) {
+		ans.push_back(c);
+		if (ans.size() < s2_len) continue;
+		bool flag = true;
+		for (int i = 0; i < s2_len; i++) {
+			if (ans[ans.size() - 1 - i] != s2[s2_len - 1 - i]) {
+				flag = false;
+				break;
+			}
+		}
+		if (!flag) continue;
+		for (int i = 0; i < s2_len; i++) ans.pop_back();
+	}
+	if (ans.empty()) cout << "FRULA";
+	else for (auto c : ans) cout << c;
+}
