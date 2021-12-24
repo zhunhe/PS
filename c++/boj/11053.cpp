81aa757e76c0b18cc7097e93733318d86bec99b8
zhunhe <xuzhunhe@gmail.com>
2021-12-24 21:42:51 +0900

    refactor: Update 11053.cpp
    - Change code indent

--- c++/boj/11053.cpp
+++ c++/boj/11053.cpp
@@ -8,16 +8,16 @@
 #include <algorithm>
 
 int main() {
-    int n;	std::cin >> n;
+	int n;	std::cin >> n;
 	std::vector<int> a(n);
-    for (int i = 0; i < n; i++) std::cin >> a[i];
+	for (int i = 0; i < n; i++) std::cin >> a[i];
 	std::vector<int> dp(n, 1);
-    for (int i = 0; i < n; i++) {
-        for (int j = i - 1; j >= 0; j--) {
-            if (a[j] >= a[i]) continue;
-            if (dp[j] < dp[i]) continue;
-            dp[i] = dp[j] + 1;
-        }
-    }
-    std::cout << *std::max_element(dp.begin(), dp.end());
+	for (int i = 0; i < n; i++) {
+		for (int j = i - 1; j >= 0; j--) {
+			if (a[j] >= a[i]) continue;
+			if (dp[j] < dp[i]) continue;
+			dp[i] = dp[j] + 1;
+		}
+	}
+	std::cout << *std::max_element(dp.begin(), dp.end());
 }
