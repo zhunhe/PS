65f917318f5a6729a9e030fde63e3fbad9e1999e
zhunhe <xuzhunhe@gmail.com>
2021-12-25 23:31:01 +0900

    feat: Solve boj 9252 dynamic programming problem

---
+++ c++/boj/9252.cpp
@@ -0,0 +1,27 @@
+/*
+ * LCS 2
+ * https://www.acmicpc.net/problem/9252
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	string s1;	cin >> s1;	s1 = " " + s1;
+	string s2;	cin >> s2;	s2 = " " + s2;
+	vector<vector<string> > dp(s1.length(), vector<string>(s2.length()));
+	for (int i = 1; i < s1.length(); i++) {
+		for (int j = 1; j < s2.length(); j++) {
+			if (s1[i] == s2[j])
+				dp[i][j] = dp[i - 1][j - 1] + s1[i];
+			else if (dp[i][j - 1].length() >= dp[i - 1][j].length())
+				dp[i][j] = dp[i][j - 1];
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
+	cout << dp[s1.length() - 1][s2.length() - 1].length() << '\n';
+	cout << dp[s1.length() - 1][s2.length() - 1];
+}
