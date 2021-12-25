e2fdafa79a3517896f08fd65a5102c4197cd74ea
zhunhe <xuzhunhe@gmail.com>
2021-12-25 22:16:05 +0900

    refactor: Update 2293.cpp
    - Delete FAST_IO
    - Delete namespace std
    - Delete #include <alogrithm>

--- c++/boj/2293.cpp
+++ c++/boj/2293.cpp
@@ -3,21 +3,17 @@
  * https://www.acmicpc.net/problem/2293
  */
 
-#define FAST_IO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
 #include <iostream>
-#include <algorithm>
 #include <vector>
-using namespace std;
 
 int main() {
-	FAST_IO();
-	int n, k;	cin >> n >> k;
-	vector<int> coin(n);
-	for (int i = 0; i < n; i++)	cin >> coin[i];
-	vector<int> dp(k + 1);
+	int n, k;	std::cin >> n >> k;
+	std::vector<int> coin(n);
+	for (int i = 0; i < n; i++)	std::cin >> coin[i];
+	std::vector<int> dp(k + 1);
 	dp[0] = 1;
 	for (int i = 0; i < n; i++)
 		for (int j = coin[i]; j <= k; j++)
 			dp[j] += dp[j - coin[i]];
-    cout << dp[k];
+    std::cout << dp[k];
 }
