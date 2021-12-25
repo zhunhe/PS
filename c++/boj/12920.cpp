98f60cf08b89d9dc4bfce3a7b3c9f52d060d5201
zhunhe <xuzhunhe@gmail.com>
2021-12-25 22:33:59 +0900

    feat: Solve boj 12920 dynamic programming problem

---
+++ c++/boj/12920.cpp
@@ -0,0 +1,27 @@
+/*
+ * Æò¹üÇÑ ¹è³¶ 2
+ * https://www.acmicpc.net/problem/12920
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	int n, m;	cin >> n >> m;
+	vector<int> dp(m + 1);
+	for (int i = 0; i < n; i++) {
+		int v, c, k;	cin >> v >> c >> k;
+		int t = 1;
+		int temp;
+		while (k > 0) {
+			temp = min(k, t);
+			for (int j = m; j >= v * temp; j--)
+				dp[j] = max(dp[j - v * temp] + c * temp, dp[j]);
+			t *= 2;
+			k -= temp;
+		}
+	}
+	cout << *max_element(dp.begin(), dp.end());
+}
