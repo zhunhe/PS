3673f3ad526dadc759ec1a4ed9a93cc5b5d5afdd
zhunhe <xuzhunhe@gmail.com>
2021-12-26 10:10:20 +0900

    feat: Solve boj 1890 dynamic programming problem

---
+++ c++/boj/1890.cpp
@@ -0,0 +1,24 @@
+/*
+ * มกวม
+ * https://www.acmicpc.net/problem/1890
+ */
+
+#include <iostream>
+
+long long board[110][110];
+long long dp[110][110];
+
+int main() {
+	int n;	std::cin >> n;
+	for (int i = 0; i < n * n; i++) std::cin >> board[i/n][i%n];
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == n-1 && j == n-1) break;
+			if (!dp[i][j]) continue;
+			dp[i + board[i][j]][j] += dp[i][j];
+			dp[i][j + board[i][j]] += dp[i][j];
+		}
+	}
+	std::cout << dp[n - 1][n - 1];
+}
