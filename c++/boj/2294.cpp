3f8f7a1260b1c05c5861055e01ed61865b353dfb
zhunhe <xuzhunhe@gmail.com>
2021-12-28 19:18:01 +0900

    feat: Solve boj 2294 dynamic programming problem

---
+++ c++/boj/2294.cpp
@@ -0,0 +1,24 @@
+/*
+ * µ¿Àü 2
+ * https://www.acmicpc.net/problem/2294
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, k;	cin >> n >> k;
+	vector<int> coin(n);
+	for (int &elem : coin) cin >> elem;
+	vector<int> dp(k + 1, 1e9);
+	dp[0] = 0;
+	for (int value : coin) {
+		for (int j = value; j <= k; j++) {
+			dp[j] = min(dp[j], dp[j - value] + 1);
+		}
+	}
+	if (dp[k] == 1e9) cout << -1;
+	else cout << dp[k];
+};
