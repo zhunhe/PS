d3b747b1427fdbe2729367917382d557d0b99e0d
zhunhe <xuzhunhe@gmail.com>
2021-12-29 06:10:36 +0900

    refactor: Update 2294.cpp
    - Delete brace

--- c++/boj/2294.cpp
+++ c++/boj/2294.cpp
@@ -14,11 +14,9 @@ int main() {
 	for (int &elem : coin) cin >> elem;
 	vector<int> dp(k + 1, 1e9);
 	dp[0] = 0;
-	for (int value : coin) {
-		for (int j = value; j <= k; j++) {
+	for (int value : coin)
+		for (int j = value; j <= k; j++)
 			dp[j] = min(dp[j], dp[j - value] + 1);
-		}
-	}
 	if (dp[k] == 1e9) cout << -1;
 	else cout << dp[k];
 };
