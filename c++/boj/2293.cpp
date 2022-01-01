9e29985e8a2f3d154ccfdf4cbc3ae3b0f7b0c121
zhunhe <xuzhunhe@gmail.com>
2022-01-02 04:16:00 +0900

    refactor: Update 2293.cpp
    - Use namespace std

--- c++/boj/2293.cpp
+++ c++/boj/2293.cpp
@@ -4,15 +4,16 @@
  */
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	int n, k;	std::cin >> n >> k;
-	std::vector<int> coin(n);
-	for (int i = 0; i < n; i++)	std::cin >> coin[i];
-	std::vector<int> dp(k + 1);
+	int n, k;	cin >> n >> k;
+	vector<int> coin(n);
+	for (int i = 0; i < n; i++)	cin >> coin[i];
+	vector<int> dp(k + 1);
 	dp[0] = 1;
 	for (int i = 0; i < n; i++)
 		for (int j = coin[i]; j <= k; j++)
 			dp[j] += dp[j - coin[i]];
-    std::cout << dp[k];
+    cout << dp[k];
 }
