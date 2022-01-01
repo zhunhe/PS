ac20c8f50a5d682507fc6c781b837c257c56ea4e
zhunhe <xuzhunhe@gmail.com>
2022-01-02 04:48:09 +0900

    refactor: Update 2630.cpp
    - Delete function

--- c++/boj/2630.cpp
+++ c++/boj/2630.cpp
@@ -9,54 +9,32 @@ using namespace std;
 #define WHITE       0
 #define BLUE        1
 
-int BOARD[130][130];
-int N;
-int W_CNT;
-int B_CNT;
+int board[130][130];
+int w_cnt, b_cnt;
 
-void set_input_data() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> BOARD[i][j];
-        }
-    }
-}
-
-// 해당 BOARD 범위에 있는 색이 모두 같은 색인지 확인
-bool check_same_color(int y, int x, int len) {
-    int check_value = BOARD[y][x];
-
-    for (int i = y; i < y + len; i++) {
-        for (int j = x; j < x + len; j++) {
-            if (check_value == BOARD[i][j]) continue;
-            return false;
-        }
-    }
+bool is_same_color(int y, int x, int len) {
+    int check_value = board[y][x];
+    for (int i = y; i < y + len; i++)
+        for (int j = x; j < x + len; j++)
+            if (check_value != board[i][j]) return false;
     return true;
 }
 
 void divide_and_conquer(int y, int x, int len) {
-    if (check_same_color(y, x, len)) {
-        if (BOARD[y][x] == WHITE) W_CNT++;
-        else B_CNT++;
+    if (is_same_color(y, x, len)) {
+        if (board[y][x] == WHITE) w_cnt++;
+        else b_cnt++;
         return ;
     }
-    // 좌상
     divide_and_conquer(y, x, len/2);
-    // 우상
     divide_and_conquer(y, x + len/2, len/2);
-    // 좌하
     divide_and_conquer(y + len/2, x, len/2);
-    // 우하
     divide_and_conquer(y + len/2, x + len/2, len/2);
 }
 
 int main() {
-    set_input_data();
-    divide_and_conquer(0, 0, N);
-    cout << W_CNT << "\n" << B_CNT;
+	int n;	cin >> n;
+	for (int i = 0; i < n * n; i++) cin >> board[i / n][i % n];
+    divide_and_conquer(0, 0, n);
+    cout << w_cnt << "\n" << b_cnt;
 }
