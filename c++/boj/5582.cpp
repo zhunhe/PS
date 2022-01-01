437971180105e18f2268702e4809d1ca7a0ff7b6
zhunhe <xuzhunhe@gmail.com>
2022-01-02 04:59:09 +0900

    refactor: Update 5582.cpp
    - Use namespace std
    - Change comment

--- c++/boj/5582.cpp
+++ c++/boj/5582.cpp
@@ -1,20 +1,21 @@
 /*
- * 괄호
+ * 공통 부분 문자열
  * https://www.acmicpc.net/problem/5582
  */
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	std::string s1;	std::cin >> s1;	s1 = " " + s1;
-	std::string s2;	std::cin >> s2;	s2 = " " + s2;
-	int max = 0;
-	std::vector<std::vector<int> > dp(s1.length(), std::vector<int>(s2.length()));
+	string s1;	cin >> s1;	s1 = " " + s1;
+	string s2;	cin >> s2;	s2 = " " + s2;
+	int ans = 0;
+	vector<vector<int> > dp(s1.length(), vector<int>(s2.length()));
 	for (int i = 1; i < s1.length(); i++)
 		for (int j = 1; j < s2.length(); j++) {
 			if (s1[i] != s2[j]) continue;
 			dp[i][j] = dp[i - 1][j - 1] + 1;
-			max = std::max(max, dp[i][j]);
+			ans = max(ans, dp[i][j]);
 		}
-    std::cout << max;
+    cout << ans;
 }
