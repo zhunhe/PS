d20afcde988d1f91c8dad6d81bcc17148ea02b66
zhunhe <xuzhunhe@gmail.com>
2022-01-02 03:54:02 +0900

    refactor: Update 1781.cpp
    - Delete functions

--- c++/boj/1781.cpp
+++ c++/boj/1781.cpp
@@ -6,22 +6,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<pair<int, long long> > v;
-priority_queue<long long> pq;
-
-void set_input_data() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int deadline;
-	long long cup_lamen;
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> deadline >> cup_lamen;
-		v.push_back(make_pair(deadline, cup_lamen));
-	}
-}
 
 bool compare(pair<int, long long> front, pair<int, long long> back) {
 	if (front.first > back.first)
@@ -29,31 +13,20 @@ bool compare(pair<int, long long> front, pair<int, long long> back) {
 	return false;
 }
 
-// ❗️문제의 데드라인과 해결시의 받을 수 있는 컵라면들의 정보가 있는 데이터를 받아
-// 최대로 받을 수 있는 컵라면 수를 구하는 문제
-long long GREEDY() {
+int main() {
+	int n;	cin >> n;
+	vector<pair<int, long long> > v(n);
+	for (auto &elem : v) cin >> elem.first >> elem.second;
+	sort(v.begin(), v.end(), compare);
 	long long answer = 0;
 	int i = 0;
-
-	// 데드라인기준으로 내림차순 정렬
-	sort(v.begin(), v.end(), compare);
-	int deadline_max = v[0].first;
-	while (deadline_max) {
-		// 현재날짜와 데드라인이 같으면 max_heap에 push
-		while (v[i].first == deadline_max) {
-			pq.push(v[i++].second);
-		}
-		// 현재날짜중 가장 컵라면을 많이 받을 수 있는 문제를 선택
+	priority_queue<long long> pq;
+	for (int deadline = v[0].first; deadline > 0; deadline--) {
+		while (v[i].first == deadline) pq.push(v[i++].second);
 		if (pq.size()) {
 			answer += pq.top();
 			pq.pop();
 		}
-		deadline_max--;
 	}
-	return answer;
-}
-
-int main() {
-	set_input_data();
-	cout << GREEDY();
+	cout << answer;
 }
