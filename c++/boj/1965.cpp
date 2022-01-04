ad5e729bddce46fb5c2013579a9a3a56b4109c9e
zhunhe <xuzhunhe@gmail.com>
2022-01-04 15:43:16 +0900

    refactor: Update 1965.cpp
    - Use namespace std
    - Use range based for loop

--- c++/boj/1965.cpp
+++ c++/boj/1965.cpp
@@ -3,18 +3,18 @@
  * https://www.acmicpc.net/problem/1965
  */
 
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+using namespace std;
 
 int main() {
-	int n;	std::cin >> n;
-	std::vector<int> box(n);
-	for (int i = 0; i < n; i++) std::cin >> box[i];
-	std::vector<int> dp(n, 1);
-	for (int i = 1; i < n; i++) {
-		for (int j = i - 1; j >= 0; j--) {
+	int n;	cin >> n;
+	vector<int> box(n), dp(n, 1);
+	for (int &elem : box) cin >> elem;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
 			if (box[j] >= box[i]) continue;
-			dp[i] = std::max(dp[i], dp[j] + 1);
+			dp[i] = max(dp[i], dp[j] + 1);
 		}
 	}
-	std::cout << *std::max_element(dp.begin(), dp.end());
+	cout << *max_element(dp.begin(), dp.end());
 }
