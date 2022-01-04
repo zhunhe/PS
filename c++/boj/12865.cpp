e233997875d2afecc8c914dbca59be3cae90fd31
zhunhe <xuzhunhe@gmail.com>
2022-01-04 16:23:56 +0900

    refactor: Update 12865.cpp
    - Use namespace std

--- c++/boj/12865.cpp
+++ c++/boj/12865.cpp
@@ -4,17 +4,20 @@
  */
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	int n, k;	std::cin >> n >> k;
-	std::vector<std::pair<int, int> > v(n);
-	for (int i = 0; i < n; i++) std::cin >> v[i].first >> v[i].second;
-	std::vector<std::vector<int> > dp(n + 1, std::vector<int>(k + 1));
+	int n, k;	cin >> n >> k;
+	vector<pair<int, int>> v(n + 1);
+	for (int i = 1; i <=n; i++) cin >> v[i].first >> v[i].second;
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
 	for (int i = 1; i <= n; i++) {
-		const int weight = v[i - 1].first;
-		const int price = v[i - 1].second;
-		for (int j = 1; j < weight; j++) dp[i][j] = dp[i - 1][j];
-		for (int j = weight; j <= k; j++) dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - weight] + price);
+		const int weight = v[i].first;
+		const int price = v[i].second;
+		for (int j = 1; j <= k; j++) {
+			if (j < weight) dp[i][j] = dp[i - 1][j];
+			else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + price);
+		}
 	}
-	std::cout << dp[n][k];
+	cout << dp[n][k];
 }
