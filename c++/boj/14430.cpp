9a7ef30b139a004936250cc8cedf11c17ce9bedf
zhunhe <xuzhunhe@gmail.com>
2022-01-04 16:10:07 +0900

    refactor: Update 14430.cpp
    - Use namespace std

--- c++/boj/14430.cpp
+++ c++/boj/14430.cpp
@@ -4,15 +4,15 @@
  */
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	int n, m;	std::cin >> n >> m;
-	std::vector<std::vector<int> > map(n + 1, std::vector<int>(m + 1, 0));
-	std::vector<std::vector<int> > dp(n + 1, std::vector<int>(m + 1, 0));
+	int n, m;	cin >> n >> m;
+	vector<vector<int>> map(n+1, vector<int>(m+1)),  dp(n+1, vector<int>(m+1));
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++) {
-			std::cin >> map[i][j];
-			dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]) + map[i][j];
+			cin >> map[i][j];
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + map[i][j];
 		}
-	std::cout << dp[n][m];
+	cout << dp[n][m];
 }
