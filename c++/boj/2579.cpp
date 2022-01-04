5af477bd37a886bedac6cb4625f5ea301e2dc160
zhunhe <xuzhunhe@gmail.com>
2022-01-04 15:55:11 +0900

    refactor: Update 2579.cpp
    - Use namespace std
    - Use range based for loop

--- c++/boj/2579.cpp
+++ c++/boj/2579.cpp
@@ -4,15 +4,15 @@
  */
 
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	int n;	std::cin >> n;
-	std::vector<int> stair(n);
-	for (int i = 0; i < n; i++) std::cin >> stair[i];
-	std::vector<int> dp(n, 1);
+	int n;	cin >> n;
+	vector<int> stair(n), dp(n, 1);
+	for (int &elem : stair) cin >> elem;
 	dp[0] = stair[0];
 	dp[1] = stair[0] + stair[1];
-	dp[2] = std::max(stair[0] + stair[2], stair[1] + stair[2]);
-    for (int i = 3; i < n; i++) dp[i] = std::max(dp[i - 2] + stair[i], stair[i - 1] + stair[i] + dp[i - 3]);
-    std::cout << dp[n - 1];
+	dp[2] = max(stair[0] + stair[2], stair[1] + stair[2]);
+    for (int i = 3; i < n; i++) dp[i] = max(dp[i - 2] + stair[i], stair[i - 1] + stair[i] + dp[i - 3]);
+    cout << dp[n - 1];
 }
