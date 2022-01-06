3cfa216f3c4e50002a2270e15af9266480c57567
zhunhe <xuzhunhe@gmail.com>
2022-01-06 19:39:17 +0900

    refactor: Update 15483.cpp
    - Use vector instead of array
    - Change range of for loop

--- c++/boj/15483.cpp
+++ c++/boj/15483.cpp
@@ -5,20 +5,20 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1002][1002];
 
 int main() {
-	string s1;	cin >> s1;	s1 = " " + s1;
-	string s2;	cin >> s2;	s2 = " " + s2;
-	for (int i = 1; i <= s1.length(); i++) dp[i][0] = i;
-	for (int i = 1; i <= s2.length(); i++) dp[0][i] = i;
-	for (int i = 1; i <= s1.length(); i++) {
-		for (int j = 1; j <= s2.length(); j++) {
+	string s1;	cin >> s1;	s1 = " " + s1; int s1_len = s1.size();
+	string s2;	cin >> s2;	s2 = " " + s2; int s2_len = s2.size();
+	vector<vector<int>> dp(s1_len, vector<int>(s2_len));
+	for (int i = 1; i < s1_len; i++) dp[i][0] = i;
+	for (int i = 1; i < s2_len; i++) dp[0][i] = i;
+	for (int i = 1; i < s1_len; i++) {
+		for (int j = 1; j < s2_len; j++) {
 			if (s1[i] == s2[j])
 				dp[i][j] = dp[i - 1][j - 1];
 			else
 				dp[i][j] = min({dp[i-1][j-1], dp[i][j-1], dp[i-1][j]}) + 1;
 		}
 	}
-	cout << dp[s1.length()][s2.length()];
+	cout << dp[s1_len-1][s2_len-1];
 }
