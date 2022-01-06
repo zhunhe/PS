b9969ff68052fa72421f354ac7d38ca9cc3decc9
zhunhe <xuzhunhe@gmail.com>
2022-01-06 12:10:57 +0900

    feat: Solve boj 12738 LIS problem

---
+++ c++/boj/12738.cpp
@@ -0,0 +1,22 @@
+/*
+ * 가장 긴 증가하는 부분 수열 3
+ * https://www.acmicpc.net/problem/12738
+ */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+	int n;	cin >> n;
+	vector<int> LIS;
+	for (int i = 0; i < n; i++) {
+		int x;	cin >> x;
+		if (LIS.empty() || x > LIS.back())
+			LIS.push_back(x);
+		else {
+			auto it = lower_bound(LIS.begin(), LIS.end(), x) - LIS.begin();
+			LIS[it] = x;
+		}
+	}
+	cout << LIS.size();
+}
