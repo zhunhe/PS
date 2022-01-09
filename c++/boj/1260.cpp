d4ecfc88bb0e49a36b94348b01adc3ae9555b34a
zhunhe <xuzhunhe@gmail.com>
2022-01-10 08:45:06 +0900

    reafctor: Update 1260.cpp
    - Change variable name

--- c++/boj/1260.cpp
+++ c++/boj/1260.cpp
@@ -6,44 +6,43 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+bool visited[1001];
 vector<vector<int>> graph;
-vector<int> check;
-int vertex, edge, start;
 
 void dfs(int now) {
-	for (int next : graph[now]) {
-		if (check[next]) continue;
-		check[next] = 1;	cout << next << ' ';	dfs(next);
-	}
+	if (visited[now]) return;
+	visited[now] = true;
+	cout << now << ' ';
+	for (int next : graph[now])
+		if (!visited[next])
+			dfs(next);
 }
 
-void bfs() {
+void bfs(int start) {
 	queue<int> q;
-	check.resize(vertex + 1);
-	check[start] = 1;	cout << start << ' ';	q.push(start);
+	q.push(start);
 	while (!q.empty()) {
 		int now = q.front(); q.pop();
-		for (int next : graph[now]) {
-			if (check[next]) continue;
-			check[next] = 1;	cout << next << ' ';	q.push(next);
-		}
+		if (visited[now]) continue;
+		visited[now] = true;
+		cout << now << ' ';
+		for (int next : graph[now])
+			q.push(next);
 	}
 }
 
 int main() {
-	cin >> vertex >> edge >> start;
-	graph.resize(vertex + 1);
-	for (int i = 0; i < edge; i++) {
-		int a, b;	cin >> a >> b;
+	int n, m, v, a, b;	cin >> n >> m >> v;
+	graph.resize(n + 1);
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
 		graph[a].push_back(b);
 		graph[b].push_back(a);
 	}
-	for (int i = 1; i <= vertex; i++)
+	for (int i = 1; i <= n; i++)
 		sort(graph[i].begin(), graph[i].end());
-	check.resize(0);	check.resize(vertex + 1, 0);
-	check[start] = 1;	cout << start << ' ';
-	dfs(start);
-	cout << '\n';
-	check.resize(0);	check.resize(vertex + 1, 0);
-	bfs();
+	dfs(v);
+	cout << "\n";
+	memset(visited, false, sizeof(visited));
+	bfs(v);
 }
