697a3344e33555a600dd566347c6527e99682eae
zhunhe <xuzhunhe@gmail.com>
2022-01-10 07:14:57 +0900

    refactor: Update 2667.cpp
    - Delete namespace std
    - Change variable name
    - Change array to vector

--- c++/boj/2667.cpp
+++ c++/boj/2667.cpp
@@ -3,44 +3,40 @@
  * https://www.acmicpc.net/problem/2667
  */
 
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+using namespace std;
 
-int n;
-int length;
-int map[25][25];
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,1,0,-1};
-std::vector<int> answer;
+int n, len;
+vector<vector<char>> coor;
+const int dy[4] = {-1,0,1,0};
+const int dx[4] = {0,1,0,-1};
 
 void DFS(int y, int x) {
 	for (int k = 0; k < 4; k++) {
 		const int ny = y + dy[k];
 		const int nx = x + dx[k];
 		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-		if (map[ny][nx] == 0) continue;
-		map[ny][nx] = 0;
-		length++;
+		if (coor[ny][nx] == '0') continue;
+		coor[ny][nx] = '0';
+		len++;
 		DFS(ny, nx);
 	}
 }
 
 int main() {
-	std::cin >> n;
-	for (int i = 0; i < n; i++) {
-		std::string s;	std::cin >> s;
-		for (int j = 0; j < n; j++)
-			map[i][j] = s[j] - '0';
+	cin >> n;
+	coor.resize(n, vector<char>(n));
+	for (int i = 0; i < n * n; i++)
+		cin >> coor[i / n][i % n];
+	vector<int> ans;
+	for (int i = 0; i < n * n; i++) {
+		if (coor[i / n][i % n] == '0') continue;
+		coor[i / n][i % n] = '0';
+		len = 1;
+		DFS(i / n, i % n);
+		ans.push_back(len);
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!map[i][j]) continue;
-			map[i][j] = 0;
-			length = 1;
-			DFS(i, j);
-			answer.push_back(length);
-		}
-	}
-	sort(answer.begin(), answer.end());
-	std::cout << answer.size() << '\n';
-	for (auto num : answer) std::cout << num << '\n';
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << '\n';
+	for (int num : ans) cout << num << '\n';
 }
