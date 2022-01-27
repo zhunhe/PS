026a21b9cfdc24b9cf2cf02b1aadabd9677c5e93
zhunhe <xuzhunhe@gmail.com>
2022-01-28 03:59:59 +0900

    refactor: Update 92334.js
    - Change unuse variable to '_'

--- js/programmers/level2/92334.js
+++ js/programmers/level2/92334.js
@@ -7,7 +7,7 @@ function solution(id_list, report, k) {
 	report = [...new Set(report)].map(a=>{return a.split(' ')});
 	const answer = [...Array(id_list.length)].fill(0);
 	const reported = new Map();
-	for (const [reporter, bad] of report)
+	for (const [_, bad] of report)
 		reported.set(bad, (reported.get(bad) || 0) + 1);
 	for (const [reporter, bad] of report)
 		if (reported.get(bad) >= k)
