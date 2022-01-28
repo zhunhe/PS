e5fc85c550ea44ac4d4aa1420972ef65d371edc8
zhunhe <xuzhunhe@gmail.com>
2022-01-28 14:46:26 +0900

    refactor: Update 2098.cpp

--- c++/boj/2098.cpp
+++ c++/boj/2098.cpp
@@ -5,33 +5,26 @@
 
 #include "bits/stdc++.h"
 using namespace std;
-#define MAX 1e9
-int n;
-vector<vector<int>> board;
-int dp[16][1 << 16];
+#define INF		987654321
+#define MAX_N	16
+int n, Graph[MAX_N][MAX_N], dp[MAX_N][1 << MAX_N];
 
-int tsp(int cur, int visited){
-	if (dp[cur][visited]) return dp[cur][visited];
+int tsp(int cur, int visited) {
 	if (visited == (1 << n) - 1)
-	{
-		if (board[cur][0] != 0)
-			return board[cur][0];
-		else
-			return MAX;
-	}
-	int result = MAX;
-	for (int i = 0; i < n; i++)
-	{
-		if (!((1 << i) & visited) && (board[cur][i] != 0))
-			result = min(result, board[cur][i] + tsp(i, visited + (1 << i)));
-	}
-	dp[cur][visited] = result;
-	return result;
+		return Graph[cur][0] ? Graph[cur][0] : INF;
+	if (dp[cur][visited]) return dp[cur][visited];
+	int &ret = dp[cur][visited];
+	ret = INF;
+	for (int next = 0; next < n; ++next)
+		if (!(visited & (1 << next)) && Graph[cur][next])
+			ret = min(ret, Graph[cur][next] + tsp(next, visited | (1 << next)));
+	return ret;
 }
 
 int main() {
 	cin >> n;
-	board.resize(n, vector<int>(n));
-	for (vector<int> &v : board) for (int &elem : v) cin >> elem;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> Graph[i][j];
 	cout << tsp(0, 1);
 }
