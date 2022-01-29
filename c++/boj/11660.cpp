96f0f0f64de684a9958d4ad1760f5c93f557532c
zhunhe <xuzhunhe@gmail.com>
2022-01-29 21:19:59 +0900

    refactor: Update 11660.cpp
    - Remove unnecessary struct coor
    - Remove unnecessary vector q
    - Simplify about dp

--- c++/boj/11660.cpp
+++ c++/boj/11660.cpp
@@ -6,31 +6,22 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define Y1		0
-#define X1		1
-#define Y2		2
-#define X2		3
-
-struct coor{
-	int y1, x1, y2, x2;
-};
-
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int n, m;	cin >> n >> m;
 	vector<vector<int>> dp(n+1, vector<int>(n+1));
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
 			cin >> dp[i][j];
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			dp[i][j] += dp[i][j-1];
-	vector<coor> q(m);
-	for (auto &v : q) cin >> v.y1 >> v.x1 >> v.y2 >> v.x2;
-	for (auto v : q) {
+			dp[i][j] += dp[i][j - 1];
+		}
+	}
+	while (m--) {
+		int y1, x1, y2, x2;
+		cin >> y1 >> x1 >> y2 >> x2;
 		int ans = 0;
-		for (int j = v.y1; j <= v.y2; j++)
-			ans += dp[j][v.x2] - dp[j][v.x1 - 1];
+		for (int i = y1; i < y2 + 1; i++)
+			ans += dp[i][x2] - dp[i][x1 - 1];
 		cout << ans << '\n';
 	}
 }
