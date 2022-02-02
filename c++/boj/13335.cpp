f4de9e757f7df1ca1f06b4ff4fce6842e95146db
zhunhe <xuzhunhe@gmail.com>
2022-02-02 12:46:01 +0900

    refactor: Change space to tab

--- c++/boj/13335.cpp
+++ c++/boj/13335.cpp
@@ -10,22 +10,22 @@ int n, w, L, t, cur_weight;
 int truck[1002];
 
 int main() {
-    cin >> n >> w >> L;
-    for (int i = 0; i < n; i++) cin >> truck[i];
-    queue<int> bridge;
-    for (int i = 0; i < n; i++) {
-        while (1) {
-            if (bridge.size() == w) {
-                cur_weight -= bridge.front();
-                bridge.pop();
-            }
-            if (cur_weight + truck[i] <= L) break;
-            bridge.push(0);
-            t++;
-        }
-        bridge.push(truck[i]);
-        cur_weight += truck[i];
-        t++;
-    }
-    cout << t + w;
+	cin >> n >> w >> L;
+	for (int i = 0; i < n; i++) cin >> truck[i];
+	queue<int> bridge;
+	for (int i = 0; i < n; i++) {
+		while (1) {
+			if (bridge.size() == w) {
+				cur_weight -= bridge.front();
+				bridge.pop();
+			}
+			if (cur_weight + truck[i] <= L) break;
+			bridge.push(0);
+			t++;
+		}
+		bridge.push(truck[i]);
+		cur_weight += truck[i];
+		t++;
+	}
+	cout << t + w;
 }
