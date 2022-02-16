9a2e18df6690a219292a3bb425433c0ecf8a8f38
zhunhe <xuzhunhe@gmail.com>
2022-02-16 20:20:45 +0900

    feat: Solve boj 9251 dynamic programming problem

---
+++ c++/boj/9251.cpp
@@ -0,0 +1,21 @@
+/*
+ * LCS
+ * https://www.acmicpc.net/problem/9251
+ */
+
+#include "bits/stdc++.h"
+using namespace std;
+int dp[1001][1001];
+
+int main() {
+	string s1, s2;	cin >> s1 >> s2;	s1 = " " + s1, s2 = " " + s2;
+	for (int i = 1; i < s1.size(); i++) {
+		for (int j = 1; j < s2.size(); j++) {
+			if (s1[i] == s2[j])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	cout << dp[s1.size() - 1][s2.size() - 1];
+}
